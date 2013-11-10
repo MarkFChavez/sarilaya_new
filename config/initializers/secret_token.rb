74027e9fd807dc9c547ad2192c7e61313c915f2b
Mark Chavez <los_populares@yahoo.com>
2013-11-09 23:57:31 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sarilaya::Application.config.secret_token = '6029ca0e6b97762003721c3741fc31e1731b0dfae812b30d06b9a9fd406ab06e26fff1119c5ec1b6c4d4fc396c73fdfea44fb48ac9f53db6e2878061ab238424'
